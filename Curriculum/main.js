73932e7b4f3ce5bbdcb62a5b18efd2dfee8f6de2
Lisandro Marquez <71771548+LisandroMarquez@users.noreply.github.com>
2021-11-11 11:31:14 -0300

    A

---
+++ Curriculum/main.js
@@ -0,0 +1,18 @@
+//Escribir ScrollReveal más cómodo
+window.sr = ScrollReveal();
+
+//ScrollReveal
+sr.reveal('.nav-main',{
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.main-photo',{
+    duration: 2000,
+    origin: 'top',
+    distance: '-200px'
+});
+sr.reveal('.photo-1'),{duration: 2000}
+sr.reveal('.photo-2'),{duration: 2000}
+sr.reveal('.photo-int'),{duration: 1500}
+sr.reveal('.photo-plan'),{duration: 1500}
